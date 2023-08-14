e514250dbd397fe08f5021c0fdd2339ef28c8015
AyshaAfnaan <ayshaafnaan23@gmail.com>
2023-08-14 20:12:37 +0530

    mydetails

---
+++ mydetails.js
@@ -0,0 +1,12 @@
+
+
+let surName = "Aysha";
+let givenname =  "Afnaan";
+let myname;
+myname = surName +" "+ givenname;
+console.log("Hey! My name is", myname);
+ 
+let age = 24;
+let presentyear = 2023;
+let yearOfBirth = presentyear - age;
+console.log("My birth year is", yearOfBirth)
