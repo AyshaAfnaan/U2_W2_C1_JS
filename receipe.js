1cd65949c5761d90f1c8c14f59b8b1658ff6ecd6
AyshaAfnaan <ayshaafnaan23@gmail.com>
2023-10-08 19:57:56 +0530

    variables and datatype

---
+++ receipe.js
@@ -0,0 +1,11 @@
+let spaghetti = 400;
+let guanciale = 250;
+let eggYork = 6;
+let pecorinoRomanoCheese = 50;
+let pepper = 4;
+let carbonara = guanciale;
+carbonara += eggYork + pecorinoRomanoCheese;
+console.log ("Combine the egg yolks", carbonara);
+
+carbonara += pepper;
+console.log("prepared", carbonara);
